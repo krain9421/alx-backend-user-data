4c9998e9e2116f8a714ab0737a071bd123290365
Nwigwe Chidubem <krain9421@gmail.com>
2024-08-01 22:50:32 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,17 +5,7 @@ from typing import List
 
 
 def filter_datum(fld: List[str], rdct: str, msg: str, sep: str) -> str:
-    """
-    returns the log message obfuscated:
-
-    Arguments:
-    fld: a list of strings representing all fields to obfuscate
-    rdct: a string representing by what the field will be obfuscated
-    msg: a string representing the log line
-    sep: a string representing by which character is separating
-        all fields in the log line
-    """
+    """Returns the log message obfuscated:"""
     for f in fld:
-        pattern = '{}=[^;]+\\{}'.format(f, sep)
-        msg = re.sub(pattern, '{}={}{}'.format(f, rdct, sep), msg)
+        msg = re.sub(f'{f}=[^;]+\\{sep}', f'{f}={rdct}{sep}', msg)
     return msg
