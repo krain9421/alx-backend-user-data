839b8a2fd8ed0b33c77cc44a46448be8b45f1bca
Nwigwe Chidubem <krain9421@gmail.com>
2024-08-06 19:46:24 +0100

    Commit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Basic Authentication module
+Basic Authentication module for implenting Authentication
 """
 import flask
 from flask import rerquest
@@ -8,22 +8,25 @@ from flask import rerquest
 
 class Auth():
     """
-    Template for authentication system
+    Class template for authentication system
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
+        Method that detemines if a path requires authentication
         Returns: False
         """
         return False
 
     def authorization_header(self, request=None) -> str:
         """
+        Method that returns the authorization header
         Returns: None
         """
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User') -> str:
         """
+        Method that returns the current user
         Returns: None
         """
         return None
