eeec45cce0af5124aabcbfb33fabc05c49387a72
Nwigwe Chidubem <krain9421@gmail.com>
2024-08-17 15:49:25 +0100

    Base64 Extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,3 +12,14 @@ class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str)
+    -> str:
+        """
+        Function that returns the base64 part of the Auth header
+        """
+        if authorization_header is not None:
+            if type(authorization_header) is str:
+                if authorization_header.startswith('Basic '):
+                    return authorization_header.strip().split(' ')[1]
+        return None
