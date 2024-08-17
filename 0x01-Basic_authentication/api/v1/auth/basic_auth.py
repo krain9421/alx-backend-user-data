0d272f4d684a79094c08d61820811fce2c7cb9f6
Nwigwe Chidubem <krain9421@gmail.com>
2024-08-17 16:29:26 +0100

    Base64 Extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Basic Auth module for implementing Basic Authentication
 from .auth import Auth
 from models.user import User
 import base64
+from base64 import b64decode as b64d
 from typing import TypeVar
 
 
@@ -22,4 +23,17 @@ class BasicAuth(Auth):
             if type(authorization_header) is str:
                 if authorization_header.startswith('Basic '):
                     return authorization_header.strip().split(' ')[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Function that decodes the base64 part of the Auth header
+        """
+        if base64_authorization_header is not None:
+            if type(base64_authorization_header) is str:
+                try:
+                    return b64d(base64_authorization_header).decode('ascii')
+                except Exception:
+                    return None
         return None
